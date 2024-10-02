abb8f37d1d6473b554839799b4ba8fccb82b3cfb
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-10-02 12:13:36 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,73 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        // Rajouter ou supprimer vos couleur ici 
+        // neutral = blanc vers noir
+        neutral100 : "var(--color-neutral-100)",
+        neutral200 : "var(--color-neutral-200)",
+        neutral300 : "var(--color-neutral-300)",
+        neutral400 : "var(--color-neutral-400)",
+        neutral500 : "var(--color-neutral-500)",
+        neutral600 : "var(--color-neutral-600)",
+        neutral700 : "var(--color-neutral-700)",
+        neutral800 : "var(--color-neutral-800)",
+        neutral900 : "var(--color-neutral-900)",
+        //primary = bleu
+        blue100 : "var(--color-primary-100)",
+        blue200 : "var(--color-primary-200)",
+        blue300 : "var(--color-primary-300)",
+        blue400 : "var(--color-primary-400)",
+        blue500 : "var(--color-primary-500)",
+        blue600 : "var(--color-primary-600)",
+        blue700 : "var(--color-primary-700)",
+        blue800 : "var(--color-primary-800)",
+        blue900 : "var(--color-primary-900)",
+        //secondary = rose
+        pink100 : "var(--color-secondary-100)",
+        pink200 : "var(--color-secondary-200)",
+        pink300 : "var(--color-secondary-300)",
+        pink400 : "var(--color-secondary-400)",
+        pink500 : "var(--color-secondary-500)",
+        pink600 : "var(--color-secondary-600)",
+        pink700 : "var(--color-secondary-700)",
+        pink800 : "var(--color-secondary-800)",
+        pink900 : "var(--color-secondary-900)",
+        //success = vert
+        green100 : "var(--color-success-100)",
+        green200 : "var(--color-success-200)",
+        green300 : "var(--color-success-300)",
+        green400 : "var(--color-success-400)",
+        green500 : "var(--color-success-500)",
+        green600 : "var(--color-success-600)",
+        green700 : "var(--color-success-700)",
+        green800 : "var(--color-success-800)",
+        green900 : "var(--color-success-900)",
+        //warning = jaune
+        yellow100 : "var(--color-warning-100)",
+        yellow200 : "var(--color-warning-200)",
+        yellow300 : "var(--color-warning-300)",
+        yellow400 : "var(--color-warning-400)",
+        yellow500 : "var(--color-warning-500)",
+        yellow600 : "var(--color-warning-600)",
+        yellow700 : "var(--color-warning-700)",
+        yellow800 : "var(--color-warning-800)",
+        yellow900 : "var(--color-warning-900)",
+        //danger = rouge
+        red100 : "var(--color-danger-100)",
+        red200 : "var(--color-danger-200)",
+        red300 : "var(--color-danger-300)",
+        red400 : "var(--color-danger-400)",
+        red500 : "var(--color-danger-500)",
+        red600 : "var(--color-danger-600)",
+        red700 : "var(--color-danger-700)",
+        red800 : "var(--color-danger-800)",
+        red900 : "var(--color-danger-900)",
+      }
+    },
+  },
+  plugins: [],
+}
+
